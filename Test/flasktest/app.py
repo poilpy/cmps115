dde46b1ddf07969cc54e9b1be814a2df392e80c8
poilpy <colinmurphy97@gmail.com>
2019-07-21 16:51:40 -0700

    Seg and Class working

--- Test/flasktest/app.py
+++ Test/flasktest/app.py
@@ -1,9 +1,27 @@
 from flask import Flask, request, render_template, redirect, url_for
+import os
+
+photos_folder = os.path.join('static', 'photos')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = photos_folder
 
 # from commons import get_tensor
-from inference import classify
+from inference import classify, segment
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 
 # Upload image page, Get and Post
 @app.route('/', methods=['GET', 'POST'])
@@ -22,14 +40,26 @@ def hello_world():
         image = file.read()
 
         resultClass = classify(image)
-        return redirect(url_for('getResult', resultClass=resultClass))
-        # return redirect(url_for('getResult'))
+        # resultClass = 'hello'
+        resultSeg = segment(image)
+        print(resultSeg)
+        # resultSeg = os.path.join("img.jpg")
+        # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+        full_filename = os.path.join("/", app.config['UPLOAD_FOLDER'], 'file.jpg')
+        # print(full_filename)
+        # resultSeg.convert('RGB').save(full_filename)
+        resultSeg.convert('RGB').save("static/photos/coolbeans.jpg")
+        # resultSeg.save(full_filename)
 
-@app.route('/result/<resultClass>', methods=['GET', 'POST'])
-def getResult(resultClass='beans'):
+        return redirect(url_for('getResult', resultClass=resultClass, resultSeg='coolbeans.jpg'))
+
+@app.route('/result/<resultClass>/<resultSeg>', methods=['GET', 'POST'])
+def getResult(resultClass='beans', resultSeg=os.path.join('/', app.config['UPLOAD_FOLDER'], 'donnie.jpg')):
     # get page
     if request.method == 'GET':
-        return render_template('result.html', flower=resultClass)
+        full_filename = os.path.join('/', app.config['UPLOAD_FOLDER'], resultSeg)
+        # full_filename = resultSeg
+        return render_template('result.html', flower=resultClass, seg=full_filename)
     if request.method == 'POST':
         return redirect(url_for('hello_world'))
 
@@ -41,4 +71,4 @@ def getResult(resultClass='beans'):
 #         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= '0.0.0.0')
--- Test/flasktest/inference.py
+++ Test/flasktest/inference.py
@@ -41,29 +41,27 @@ def classify(img):
 
 
 def segment(img):
+	img = get_tensor(img).cuda()
 	model = torch.load('segSaveModel.pth')
-	with open(img, encoding="utf8", errors='ignore') as f:
-  		input_image = f.read()
-	# input_image = Image.open(img)
-	preprocess = transforms.Compose([
-	    transforms.ToTensor(),
-	    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-	])
+	# preprocess = transforms.Compose([
+	#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+	# ])
 
 	print('lol')
 
-	input_tensor = preprocess(input_image)
-	input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+	# input_tensor = preprocess(input_image)
+	# input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+	# input_batch = input_image.unsqueeze(0)
 
 	# move the input and model to GPU for speed if available
 	if torch.cuda.is_available():
-	    input_batch = input_batch.to('cuda')
+	    input_batch = img.to('cuda')
 	    model.to('cuda')
 
 	with torch.no_grad():
 	    output = model(input_batch)['out'][0]
 	output_predictions = output.argmax(0)
-
+	# r =output_predictions
 
 
 	palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
@@ -71,7 +69,7 @@ def segment(img):
 	colors = (colors % 255).numpy().astype("uint8")
 
 	# plot the semantic segmentation predictions of 21 classes in each color
-	r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
+	r = Image.fromarray(output_predictions.byte().cpu().numpy())
 	r.putpalette(colors)
 
 	return r
