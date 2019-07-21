050d0e96091f21e84bb2cb1fe6db3e1e0b8a844f
poilpy <colinmurphy97@gmail.com>
2019-07-21 14:46:34 -0700

    Adding segment

--- Test/flasktest/inference.py
+++ Test/flasktest/inference.py
@@ -3,6 +3,9 @@ import json
 from commons import get_tensor
 import torch
 
+from PIL import Image
+from torchvision import transforms
+
 # with open('cat_to_name.json') as f:
 # 	cat_to_name = json.load(f)
 
@@ -33,3 +36,42 @@ def classify(img):
 	cool = classes[predicted[0]]
 
 	return cool
+
+
+
+
+def segment(img):
+	model = torch.load('segSaveModel.pth')
+	with open(img, 'rb') as f:
+  		input_image = f.read()
+	# input_image = Image.open(img)
+	preprocess = transforms.Compose([
+	    transforms.ToTensor(),
+	    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+	])
+
+	print('lol')
+
+	input_tensor = preprocess(input_image)
+	input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+	# move the input and model to GPU for speed if available
+	if torch.cuda.is_available():
+	    input_batch = input_batch.to('cuda')
+	    model.to('cuda')
+
+	with torch.no_grad():
+	    output = model(input_batch)['out'][0]
+	output_predictions = output.argmax(0)
+
+
+
+	palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+	colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+	colors = (colors % 255).numpy().astype("uint8")
+
+	# plot the semantic segmentation predictions of 21 classes in each color
+	r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
+	r.putpalette(colors)
+
+	return r
